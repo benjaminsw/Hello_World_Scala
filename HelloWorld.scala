154e78b086632b5a8b0b97c8c0e32eda55c92202
benjaminsw <benjaminsw@live.com>
2016-03-15 12:05:04 +0000

    First submission
    First scala program

---
+++ HelloWorld.scala
@@ -0,0 +1,7 @@
+package Hello_World_Scala
+//this is to test my first Scala program and to verify if I have the correct environment to run Scala 
+object HelloWorld {
+   def main(args: Array[String]): Unit = {
+    println("Hello, world!")
+  }
+}
