085b0942c22c336dc1dfea62cff227d0b701a3b7
benjaminsw <benjaminsw@live.com>
2016-03-15 17:33:45 +0000

    added comment

--- HelloWorld.scala
+++ HelloWorld.scala
@@ -2,6 +2,6 @@ package Hello_World_Scala
 //this is to test my first Scala program and to verify if I have the correct environment to run Scala 
 object HelloWorld {
    def main(args: Array[String]): Unit = {
-    println("Hello, world!")
+    println("Hello, world from Scala!")
   }
 }
